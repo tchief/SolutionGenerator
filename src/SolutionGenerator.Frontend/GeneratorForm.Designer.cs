8aec89996509e1f1ea9c5d833e04bb8e404aceaa
Pieter Van Parys <pieter.vanparys@gmail.com>
2013-11-25 07:12:36 +0100

    Fixed form width

--- src/SolutionGenerator.Frontend/GeneratorForm.Designer.cs
+++ src/SolutionGenerator.Frontend/GeneratorForm.Designer.cs
@@ -510,7 +510,7 @@ namespace SolutionGenerator.Frontend
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(818, 478);
+            this.ClientSize = new System.Drawing.Size(736, 478);
             this.Controls.Add(projectOutputTypeLabel);
             this.Controls.Add(this.projectOutputTypeComboBox);
             this.Controls.Add(initiliazeGitLabel);
